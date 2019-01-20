a70dac9d8c343f5223323333baef3d51a780ae51
Bacancy-avani <avani.j.patel@bacancytechnology.com>
2019-01-20 18:31:20 +0530

    last modification

---
+++ main.js
@@ -0,0 +1,67 @@
+function getContacts() {
+    $.ajax({
+        type : 'GET',
+        url:'http://localhost:8080/api/contacts/',
+        complete: function (response) {
+            $('#output').html(response.responseText,status);
+        },
+        error: function () {
+            $('#output').html('There was an error!');
+        },
+    });
+    return false;
+}
+
+function removeContacts() {
+    $.ajax({
+        type: "DELETE",
+        url: "http://localhost:8080/api/contacts/"+$("#contactId").val(),
+        complete: function(response){
+            $("#output").html(response.responseText);
+        },
+        error: function () {
+            $('#output').html('There was an error!');
+        },
+    });
+    return false;
+}
+
+function createContacts() {
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:8080/api/contacts/",
+        data: {
+                name : $("#user_name").val(),
+                email : $("#email_address").val()
+        },
+
+        complete: function(response){
+            $('#output').html(response.responseText);
+        },
+        error: function () {
+            $('#output').html('There was an error!');
+        },
+    });
+    return false;
+}
+
+function updateContacts() {
+    $.ajax({
+        type: "PUT",
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        url: "http://localhost:8080/api/contacts/"+$('#contactId').val(),
+        data: {
+                name : $('#user_name').val(),
+                email : $('#email_address').val()
+        },
+        complete: function(response){
+            $("#output").html(response.responseText);
+        },
+        error: function () {
+            $('#output').html('There was an error!');
+        },
+    });
+    return false;
+}
